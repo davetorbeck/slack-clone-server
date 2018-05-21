f376a2445548a9afc674cc5065f77176d0f86933
David Torbeck <davetorbeck@gmail.com>
2018-05-21 00:49:54 -0700

    Add cors

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
 
 import models from './models';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 
-const resolvers = mergeResolvers(
-  fileLoader(path.join(__dirname, './resolvers'))
-);
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -21,6 +20,8 @@ const schema = makeExecutableSchema({
 
 const app = express();
 const graphqlEndpoint = '/graphql';
+
+app.use(cors('*'));
 
 app.use(
   graphqlEndpoint,
