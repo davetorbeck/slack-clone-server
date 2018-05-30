5f4f883bc2ac8c8226b87e7c31b6e74a55b978e8
David Torbeck <davetorbeck@gmail.com>
2018-05-30 13:06:49 -0700

    Team sequelize query fix

--- resolvers/team.js
+++ resolvers/team.js
@@ -3,24 +3,29 @@ import requiresAuth from '../permissions'
 
 export default {
   Query: {
-    allTeams: requiresAuth.createResolver(async (parent, args, { models, user }) => {
-      models.Team.findAll({ owner: user.id }, { raw: true })
-    }),
+    allTeams: requiresAuth.createResolver(async (parent, args, { models, user }) =>
+      models.Team.findAll({ where: { owner: user.id } }, { raw: true })
+    ),
   },
   Mutation: {
-    createTeam: requiresAuth.createResolver(async (parent, args, { models }) => {
+    createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        await models.Team.create({ ...args, owner: user.id })
-
+        const team = await models.Team.create({ ...args, owner: user.id })
+        await models.Channel.create({ name: 'general', public: true, teamId: team.id })
         return {
           ok: true,
+          team,
         }
       } catch (err) {
+        console.log(err)
         return {
           ok: false,
           errors: formatErrors(err),
         }
       }
     }),
+  },
+  Team: {
+    channels: ({ id }, args, { models }) => models.Channel.findAll({ where: { teamId: id } }),
   },
 }
