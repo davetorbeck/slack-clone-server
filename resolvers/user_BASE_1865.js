c9fb47f8ebcf48491f869b644c2328c1a47b3464
David Torbeck <davetorbeck@gmail.com>
2018-05-30 20:57:22 -0700

    Remove git backups

--- resolvers/user_BASE_1865.js
+++
@@ -1,9 +0,0 @@
-export default {
-  Query: {
-    getUser: (parent, args, { models }) => models.User.findOne({ where: { id } }),
-    allUsers: (parent, args, { models }) => models.User.findAll(),
-  },
-  Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
-  },
-};
